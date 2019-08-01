a20b9ba1e7dca57cb54198445857a59b70298a46
Joshua-owolabi <primtech.co@gmail.com>
2019-08-01 19:26:33 +0100

    added frontend vuejs

---
+++ frontend/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app>
+    <v-toolbar>
+    <v-toolbar-title>Title</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items>
+      <v-btn text>Link 1</v-btn>
+      <v-btn text>Link 2</v-btn>
+      <v-btn text>Link 3</v-btn>
+    </v-toolbar-items>
+
+    <template v-if="$vuetify.breakpoint.smAndUp">
+      <v-btn icon>
+        <v-icon>mdi-export-variant</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-delete-circle</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-plus-circle</v-icon>
+      </v-btn>
+    </template>
+  </v-toolbar>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</v-app>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
